1ea237fb03f14e1a6493ab84030bd163f19394e6
simonsfan <17802115971@163.com>
2018-06-30 20:44:03 +0800

    代码修改

--- src/main/java/com/simons/cn/springbootdemo/controller/weixin/TokenNotifyController.java
+++ src/main/java/com/simons/cn/springbootdemo/controller/weixin/TokenNotifyController.java
@@ -1,8 +1,5 @@
 package com.simons.cn.springbootdemo.controller.weixin;
 
-import com.simons.cn.springbootdemo.util.ConstantEnum;
-import com.simons.cn.springbootdemo.util.Result;
-import com.simons.cn.springbootdemo.util.ResultUtil;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,17 +24,20 @@ public class TokenNotifyController {
 
     @GetMapping("/tokencheck")
     @ResponseBody
-    public Result tokenCheck(@RequestParam(required = false, name = "signature") String signature,
+    public String tokenCheck(@RequestParam(required = false, name = "signature") String signature,
                              @RequestParam(required = false, name = "timestamp") String timestamp,
                              @RequestParam(required = false, name = "nonce") String nonce,
-                             @RequestParam(required = false, name = "echostr") String echostr
-    ) {
+                             @RequestParam(required = false, name = "echostr") String echostr) {
         logger.info("signature=" + signature + ",timestamp=" + timestamp + ",nonce=" + nonce + ",echostr" + echostr);
-        List<String> list = Arrays.asList(timestamp, nonce, echostr);
+        String token = "jkw932465";
+        List<String> list = Arrays.asList(token, timestamp, nonce);
         Collections.sort(list);
         String shahex = DigestUtils.shaHex(list.get(0) + list.get(1) + list.get(2));
         logger.info("加密后的签名signature={}", shahex);
-        return ResultUtil.success(ConstantEnum.SUCCESS.getCode(), ConstantEnum.SUCCESS.getMsg(), shahex);
+        if (signature.equals(shahex)) {
+            return echostr;
+        }
+        return "";
     }
 
 }
