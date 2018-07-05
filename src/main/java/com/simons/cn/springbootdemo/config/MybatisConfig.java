e0611d525e481342ae40903417dd25bbb5671afe
simonsfan <17802115971@163.com>
2018-07-05 15:17:47 +0800

    增加登录页面

---
+++ src/main/java/com/simons/cn/springbootdemo/config/MybatisConfig.java
@@ -0,0 +1,11 @@
+package com.simons.cn.springbootdemo.config;
+
+/**
+ * 项目名称：springbootdemo
+ * 类名称：com.simons.cn.springbootdemo.config
+ * 类描述：
+ * 创建人：simonsfan
+ * 创建时间：2018/7/5 14:46
+ */
+public class MybatisConfig {
+}
